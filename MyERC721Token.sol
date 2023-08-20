9594827b0b341458f92ad1240fd4d70a24fe357b
Grambeatz1 <141178999+Grambeatz1@users.noreply.github.com>
2023-08-20 22:41:44 +0200

    MyERC721Token.sol

---
+++ MyERC721Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MyERC721Token is ERC721 {
+    constructor(string memory name, string memory symbol) ERC721(name, symbol) {}
+
+    function mintToken(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
